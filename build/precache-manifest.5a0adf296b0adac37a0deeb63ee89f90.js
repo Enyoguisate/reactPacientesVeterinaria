9a6e8671809da4c1c4fbed5e8925f69200c42bfe
Enyoguisate <victormartin9086@gmail.com>
2019-11-26 14:54:46 -0300

    added docs

---
+++ build/precache-manifest.5a0adf296b0adac37a0deeb63ee89f90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4857f812cc248f152ffeac2c35269099",
+    "url": "/index.html"
+  },
+  {
+    "revision": "037a3a85cdce8f41e2e5",
+    "url": "/static/css/main.7a789ea0.chunk.css"
+  },
+  {
+    "revision": "2e3e69008109249969b8",
+    "url": "/static/js/2.6ab4248a.chunk.js"
+  },
+  {
+    "revision": "037a3a85cdce8f41e2e5",
+    "url": "/static/js/main.3ea7f3a5.chunk.js"
+  },
+  {
+    "revision": "9f95f845fe285f4351ef",
+    "url": "/static/js/runtime-main.77951f78.js"
+  }
+]);
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import '../bootstrap.min.css';
+import PropTypes from 'prop-types';
 
 const Header = ({titulo}) => ( 
     <header>
         <h1 className="text-center">{titulo}</h1>
     </header>
 );
+
+Header.prototype = {
+    titulo : PropTypes.string.isRequired
+};
 
 export default Header;
